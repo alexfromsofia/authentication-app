b959e5fc8f98764de10d4ae2a38f5912ccd7b0d4
Alex <alexander@tradeo.com>
2018-11-28 15:46:21 +0200

    Add signin

---
+++ client/src/components/auth/Signin.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+import { signin } from '../../actions';
+
+class Signin extends Component {
+  onSubmit = (formProps) => {
+    this.props.signin(formProps, () => {
+      this.props.history.push('/feature')
+    });
+  }
+
+  render() {
+    const { errorMessage, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <fieldset>
+          <label>Email</label>
+          <Field 
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
+        </fieldset>
+        <div className="error-message">
+          { errorMessage }
+        </div>
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  errorMessage: state.auth.errorMessage
+});
+
+export default compose(
+  connect(mapStateToProps, { signin }),
+  reduxForm({ form: 'signin' }),
+)(Signin);
